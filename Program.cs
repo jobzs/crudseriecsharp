901307f4a18e91402d4f248b8f6f7fcad06429da
Jobz <joberson@meow.digital>
2022-01-23 21:39:21 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,173 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace CRUD.Series
+{
+    class Program
+    {
+        static SerieRepositorio repositorio = new SerieRepositorio();
+        static void Main(string[] args)
+        {
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        ListarSeries();
+                        break;
+                    case "2":
+                        InserirSerie();
+                        break;
+                    case "3":
+                        AtualizarSerie();
+                        break;
+                    case "4":
+                        ExcluirSerie();
+                        break;
+                    case "5":
+                        VisualizarSerie();
+                        break;
+                    case "C":
+                        Console.Clear();
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+            System.Console.WriteLine("Obrigado por utilizar nossos serviços.");
+            Console.ReadLine();
+        }
+
+
+        private static void ListarSeries()
+        {
+            System.Console.WriteLine("Listar séries");
+
+            var lista = repositorio.Lista();
+
+            if (lista.Count == 0)
+            {
+                Console.WriteLine("Nenhuma série cadastrada.");
+                return;
+            }
+
+            foreach (var serie in lista)
+            {
+                var excluido = serie.retornaExcluido();
+                if(excluido == false)
+                {
+                    System.Console.WriteLine("#ID {0}: - {1}", serie.retornaId(), serie.retornaTitulo());
+                    System.Console.WriteLine("Ano: {0}: - Gênero: {1}", serie.retornaAno(), serie.retornaGenero());
+                    System.Console.WriteLine($"Descrição:  {serie.retornaDescricao()}");
+                    System.Console.WriteLine();
+                }
+
+                
+            }
+        }
+        private static void InserirSerie()
+        {
+            System.Console.WriteLine("Inserir nova série");
+
+            foreach (int i in Enum.GetValues(typeof(Genero)))
+            {
+                System.Console.WriteLine("{0} - {1}", i, Enum.GetName(typeof(Genero), i));
+            }
+            System.Console.WriteLine("Digite o gênero entre as opções acima: ");
+            int entradaGenero = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("Digite o Título da Série: ");
+            string entradaTitulo = Console.ReadLine();
+
+            System.Console.WriteLine("Digite o Ano de Inicio da Série: ");
+            int entradaAno = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("Digite a Descrição da Série: ");
+            string entradaDescricao = Console.ReadLine();
+
+            Serie novaSerie = new Serie(
+                id: repositorio.ProximoId(),
+                genero: (Genero)entradaGenero,
+                titulo: entradaTitulo,
+                ano: entradaAno,
+                descricao: entradaDescricao);
+
+            repositorio.Insere(novaSerie);
+        }
+        private static void AtualizarSerie()
+        {
+            Console.Write("Digite o id da série: ");
+            int indiceSerie = int.Parse(Console.ReadLine());
+
+            foreach (int i in Enum.GetValues(typeof(Genero)))
+            {
+                Console.WriteLine("{0}-{1}", i, Enum.GetName(typeof(Genero), i));
+            }
+            Console.Write("Digite o gênero entre as opções acima: ");
+            int entradaGenero = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o Título da Série: ");
+            string entradaTitulo = Console.ReadLine();
+
+            Console.Write("Digite o Ano de Início da Série: ");
+            int entradaAno = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a Descrição da Série: ");
+            string entradaDescricao = Console.ReadLine();
+
+            Serie atualizaSerie = new Serie(
+                id: indiceSerie,
+                genero: (Genero)entradaGenero,
+                titulo: entradaTitulo,
+                ano: entradaAno,
+                descricao: entradaDescricao);
+
+            repositorio.Atualiza(indiceSerie, atualizaSerie);
+        }
+
+        private static void ExcluirSerie()
+        {
+            Console.Write("Digite o id da série: ");
+            int indiceSerie = int.Parse(Console.ReadLine());
+
+            repositorio.Exclui(indiceSerie);
+        }
+        private static void VisualizarSerie()
+        {
+            Console.Write("Digite o id da série: ");
+            int indiceSerie = int.Parse(Console.ReadLine());
+
+            var serie = repositorio.RetornaPorId(indiceSerie);
+
+            Console.WriteLine(serie);
+        }
+
+
+        private static string ObterOpcaoUsuario()
+        {
+            System.Console.WriteLine();
+            System.Console.WriteLine("Séries para você!");
+            System.Console.WriteLine("Informe a opção desejada: ");
+
+            System.Console.WriteLine("1- Listar séries");
+            System.Console.WriteLine("2- Inserir nova série");
+            System.Console.WriteLine("3- Atualizar série");
+            System.Console.WriteLine("4- Excluir série");
+            System.Console.WriteLine("5- Visualizar série");
+            System.Console.WriteLine("C- Limpar Tela");
+            System.Console.WriteLine("X-Sair");
+            System.Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
+
+    }
+}
+
+
